a38a50cfd3066fd84e4516ad74f78f18625328cc
kthiriyoussef <104700801+kthiriyoussef@users.noreply.github.com>
2023-11-18 13:42:32 +0100

    Update voiture.service.ts

--- src/app/voiture.service.ts
+++ src/app/voiture.service.ts
@@ -13,65 +13,67 @@ import { AuthService } from './auth.service'; const httpOptions = {headers: new 
   providedIn: 'root'
 })
 export class VoitureService {
+  hostUrl='http://localhost:8080';
   constructor(private http: HttpClient,private authService:AuthService,) { }
   getVoiture(): Observable<voiture[]>{
     
-    return this.http.get<voiture[]>(environment.hostUrl+'/Miniprojet/api/all');
+    return this.http.get<voiture[]>(this.hostUrl+'/Miniprojet/api/all');
   }
-  addVoiture(voiture: voiture) {
+  addVoiture(voiture: voiture):Observable<number> {
     
-    return this.http.post(environment.hostUrl+'/Miniprojet/api/addVoit', voiture);
+    return this.http.post<number>(this.hostUrl+'/Miniprojet/api/addVoiture', voiture);
   }
   getVoitureid(id: number): Observable<any> {
     
-    return this.http.get<voiture[]>(environment.hostUrl+`/Miniprojet/api/getbyid/${id}`);
+    return this.http.get<voiture[]>(this.hostUrl+`/Miniprojet/api/getbyid/${id}`);
   }
   updateVoiture( updatedObject: Object): Observable<voiture[]> {
    
-    return this.http.put<voiture[]>(environment.hostUrl+`/Miniprojet/api/updateVoit`, updatedObject);
+    return this.http.put<voiture[]>(this.hostUrl+`/Miniprojet/api/updateVoit`, updatedObject);
   }
   deleteVoiture (id: number){
    
-    return this.http.delete(environment.hostUrl+`/Miniprojet/api/delVoit/${id}`);
+    return this.http.delete(this.hostUrl+`/Miniprojet/api/delVoit/${id}`);
   }
   listeMarques():Observable<marque[]>{
     
     
-    return this.http.get<marque[]>(environment.hostUrl+"/Miniprojet/api/marque");
+    return this.http.get<marque[]>(this.hostUrl+"/Miniprojet/api/marque");
     }
     rechercherParMarque(idMar: number):Observable< voiture[]> {
       
-      return this.http.get<voiture[]>(environment.hostUrl+`/Miniprojet/api/voitMarq/${idMar}`);
+      return this.http.get<voiture[]>(this.hostUrl+`/Miniprojet/api/voitMarq/${idMar}`);
       }
       rechercherParModele(modele: String):Observable< voiture[]> {
         
-        return this.http.get<voiture[]>(environment.hostUrl+`/Miniprojet/api/voitByModel/${modele}`);
+        return this.http.get<voiture[]>(this.hostUrl+`/Miniprojet/api/voitByModel/${modele}`);
         }
   ajouterMarque( marq: marque):Observable<marque>{
     
-          return this.http.post<marque>(environment.hostUrl+`/Miniprojet/marqrest`, marq);
+          return this.http.post<marque>(this.hostUrl+`/Miniprojet/marqrest`, marq);
           }
           uploadImage(file: File, filename: string): Observable<Image>{
             const imageFormData = new FormData();
             imageFormData.append('image', file, filename);
-            return this.http.post<Image>(environment.hostUrl+`/Miniprojet/api/image/upload`, imageFormData);
+            return this.http.post<Image>(this.hostUrl+`/Miniprojet/api/image/upload`, imageFormData);
             }
             loadImage(id: number): Observable<Image> {
               
-              return this.http.get<Image>(environment.hostUrl+ `/Miniprojet/api/image/get/info/${id}`);
+              return this.http.get<Image>(this.hostUrl+ `/Miniprojet/api/image/get/info/${id}`);
               }
               uploadImageVoit(file: File, filename: string, idVoit:number): Observable<any>{
                 const imageFormData = new FormData();
                 imageFormData.append('image', file, filename);
                 
-                return this.http.post(environment.hostUrl+`/Miniprojet/api/image/uplaodImageVoit/${idVoit}`, imageFormData);
+                return this.http.post(this.hostUrl+`/Miniprojet/api/image/uplaodImageVoit/${idVoit}`, imageFormData);
                 }
                 supprimerImage(id : number) {
-                  return this.http.delete(environment.hostUrl+`/Miniprojet/api/image/delete/${id}`, httpOptions);
+                  return this.http.delete(this.hostUrl+`/Miniprojet/api/image/delete/${id}`, httpOptions);
                   }
               getImagesVoit(id:number): Observable<Image>{
-                return  this.http.get<Image>(environment.hostUrl+ `/Miniprojet/api/image/getImagesVoit/${id}`);
+                return  this.http.get<Image>(this.hostUrl+ `/Miniprojet/api/image/getImagesVoit/${id}`);
               }
+            
             
   
 }
